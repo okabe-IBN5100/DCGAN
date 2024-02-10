a8f5c27b2d21ca5f73c7813a0d3c14478eab3046
Samruddha <samruddhahajare@gmail.com>
2024-02-10 23:07:34 +0530

    updated-hyperparameters

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import torch
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 CHANNELS = 64
-EPOCHS = 5
-LEARNING_RATE = 0.002
+EPOCHS = 1
+LEARNING_RATE = 0.0002
 BATCH_SIZE = 128
+BETA1 = 0.5
