d0259a68bfd4f0f48b652f35b64b5b101321a60e
Samruddha <samruddhahajare@gmail.com>
2024-02-10 23:07:08 +0530

    added-batch-norm

--- model.py
+++ model.py
@@ -7,20 +7,24 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
 
         self.model = nn.Sequential(
-            nn.ConvTranspose2d(100, CHANNELS*16, 4, 1, 0), # (1, 100) -> (64*16, 4, 4)
+            nn.ConvTranspose2d(100, CHANNELS*16, 4, 1, 0, bias=False), # (100, 1) -> (64*16, 4, 4)
+            nn.BatchNorm2d(CHANNELS * 16),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(CHANNELS*16, CHANNELS*8, 4, 2, 1), # (64*16, 4, 4) -> (64*8, 8, 8)
+            nn.ConvTranspose2d(CHANNELS*16, CHANNELS*8, 4, 2, 1, bias=False), # (64*16, 4, 4) -> (64*8, 8, 8)
+            nn.BatchNorm2d(CHANNELS * 8),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(CHANNELS*8, CHANNELS*4, 4, 2, 1), # (64*8, 8, 8) -> (64*4, 16,16)
+            nn.ConvTranspose2d(CHANNELS*8, CHANNELS*4, 4, 2, 1, bias=False), # (64*8, 8, 8) -> (64*4, 16,16)
+            nn.BatchNorm2d(CHANNELS * 4),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(CHANNELS*4, CHANNELS*2, 4, 2, 1), # (64*4, 16, 16) -> (64*2, 32, 32)
+            nn.ConvTranspose2d(CHANNELS*4, CHANNELS*2, 4, 2, 1, bias=False), # (64*4, 16, 16) -> (64*2, 32, 32)
+            nn.BatchNorm2d(CHANNELS * 2),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(CHANNELS*2, 3, 4, 2, 1), # (64*2, 32, 32) -> (3, 64, 64)
-            nn.ReLU()
+            nn.ConvTranspose2d(CHANNELS*2, 3, 4, 2, 1, bias=False), # (64*2, 32, 32) -> (3, 64, 64)
+            nn.Tanh()
         )
 
     def forward(self, X):
@@ -31,24 +35,36 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
         
         self.model = nn.Sequential(
-            nn.Conv2d(3, CHANNELS, 4, 2, 1), # (3, 64, 64) -> (64, 32, 32)
+            nn.Conv2d(3, CHANNELS, 4, 2, 1, bias=False), # (3, 64, 64) -> (64, 32, 32)
+            nn.BatchNorm2d(CHANNELS),
             nn.ReLU(),
 
-            nn.Conv2d(CHANNELS, CHANNELS*2, 4, 2, 1), # (64, 32, 32) -> (128, 16, 16)
+            nn.Conv2d(CHANNELS, CHANNELS*2, 4, 2, 1, bias=False), # (64, 32, 32) -> (128, 16, 16)
+            nn.BatchNorm2d(CHANNELS * 2),
             nn.ReLU(),
 
-            nn.Conv2d(CHANNELS*2, CHANNELS*4, 4, 2, 1), # (128, 16, 16) -> (256, 8, 8)
+            nn.Conv2d(CHANNELS*2, CHANNELS*4, 4, 2, 1, bias=False), # (128, 16, 16) -> (256, 8, 8)
+            nn.BatchNorm2d(CHANNELS * 4),
             nn.ReLU(),
 
-            nn.Conv2d(CHANNELS*4, CHANNELS*8, 4, 2, 1), # (256, 8, 8) -> (512, 4, 4)
+            nn.Conv2d(CHANNELS*4, CHANNELS*8, 4, 2, 1, bias=False), # (256, 8, 8) -> (512, 4, 4)
+            nn.BatchNorm2d(CHANNELS * 8),
             nn.ReLU(),
 
-            nn.Conv2d(CHANNELS*8, 1, 4, 1, 0), # (512, 4, 4) -> (1, 1, 1)
-            nn.Sigmoid(),
+            nn.Conv2d(CHANNELS*8, 1, 4, 1, 0, bias=False), # (512, 4, 4) -> (1, 1, 1)
+            nn.Sigmoid()
         )
 
     def forward(self, X):
         return self.model(X)
+    
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
     
 
 if __name__ == "__main__":
